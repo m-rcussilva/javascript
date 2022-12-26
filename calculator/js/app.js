7907bacfbda240806e25022892d0b67ccce6efbf
Marcus Silva <marcusluis.mail@gmail.com>
2022-12-26 14:49:05 -0300

    removing comments

--- calculator/js/app.js
+++ calculator/js/app.js
@@ -2,7 +2,6 @@ const num1Input = document.getElementById("num1")
 const num2Input = document.getElementById("num2")
 const resultEl = document.getElementById("result")
 
-// Define functions for each arithmetic operation
 function add(num1, num2) {
     return num1 + num2
 }
@@ -19,7 +18,6 @@ function divide(num1, num2) {
     return num1 / num2
 }
 
-// Add event listeners to the buttons
 document.getElementById("add-btn").addEventListener("click", function () {
     const num1 = Number(num1Input.value)
     const num2 = Number(num2Input.value)

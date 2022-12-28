6b0015d0ba76a1fb8c299d26af6b2ad02dfa85da
Marcus Silva <marcusluis.mail@gmail.com>
2022-12-28 10:30:18 -0300

    usando o metodo forEach para iterar sobre uma lista de el

--- calculator/js/app.js
+++ calculator/js/app.js
@@ -1,18 +1,10 @@
 const displayI = document.getElementById("display-i")
 const displayO = document.getElementById("display-o")
 
-const num0 = document.getElementById("num0")
-const num1 = document.getElementById("num1")
-const num2 = document.getElementById("num2")
-const num3 = document.getElementById("num3")
-const num4 = document.getElementById("num4")
-const num5 = document.getElementById("num5")
-const num6 = document.getElementById("num6")
-const num7 = document.getElementById("num7")
-const num8 = document.getElementById("num8")
-const num9 = document.getElementById("num9")
+const num = document.querySelectorAll("#num")
 
-num1.addEventListener("click", (e) => {
-    displayI.textContent += e.target.textContent
-    console.log(e.target.textContent)
+num.forEach((el) => {
+    el.addEventListener("click", (e) => {
+        displayI.textContent += e.target.textContent
+    })
 })

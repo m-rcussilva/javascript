f5bd7f747dada178a92e78d2da16a6b84a8e45f8
Marcus Silva <marcusluis.mail@gmail.com>
2022-12-29 19:07:44 -0300

    projeto finalizado

--- calculator/js/app.js
+++ calculator/js/app.js
@@ -2,9 +2,27 @@ const displayI = document.getElementById("display-i")
 const displayO = document.getElementById("display-o")
 
 const num = document.querySelectorAll("#num")
+const equalBtn = document.querySelector(".equal-btn")
+const clearBtn = document.querySelector(".clear-btn")
 
 num.forEach((el) => {
     el.addEventListener("click", (e) => {
         displayI.textContent += e.target.textContent
     })
+})
+
+equalBtn.addEventListener("click", () => {
+    if (displayI.textContent === "") {
+        displayI.textContent = alert("Enter a number")
+    } else {
+        // O eval() é capaz de avaliar qualquer código JS válido, incluindo expressões matemáticas
+        let result = eval(displayI.textContent)
+        displayO.textContent = displayI.textContent
+        displayO.textContent = result
+    }
+})
+
+clearBtn.addEventListener("click", () => {
+    displayI.textContent = ""
+    displayO.textContent = ""
 })
